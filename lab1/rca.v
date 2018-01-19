f694a4f021bad6454c6dbd572f704ffdbe09c7ba
Joe Devietti <devietti@seas.upenn.edu>
2018-01-18 21:26:08 -0500

    added more documentation about each module

--- lab1/rca.v
+++ lab1/rca.v
@@ -1,6 +1,12 @@
+// Tell the simulator that we express delays at nanosecond granularity, and that
+// it should track timing at picosecond granularity.
 `timescale 1ns / 1ps
+
+// prevents undeclared wires from being inferred
 `default_nettype none
 
+/* A half-adder that adds two 1-bit numbers and produces a 2-bit result (as sum
+/* and carry-out) */
 module halfadder(input wire  a,
                  input wire  b,
                  output wire s,
@@ -8,7 +14,9 @@ module halfadder(input wire  a,
    assign s = a ^ b;
    assign cout = a & b; 
 endmodule
-  
+
+/* A full adder adds three 1-bit numbers (a, b, carry-in) and produces a 2-bit
+/* result (as sum and carry-out) */
 module fulladder(input wire  cin,
                  input wire  a,
                  input wire  b,
@@ -21,6 +29,7 @@ module fulladder(input wire  cin,
    assign cout = cout_tmp1 | cout_tmp2; 
 endmodule
 
+/* A full adder that adds 2-bit numbers. Builds upon the 1-bit full adder. */
 module fulladder2(input wire        cin,
                   input wire  [1:0] a,
                   input wire  [1:0] b,
@@ -31,7 +40,10 @@ module fulladder2(input wire        cin,
    fulladder a0(.cin(cin), .a(a[0]), .b(b[0]), .s(s[0]), .cout(cout));
    fulladder a1(.cin(cout_tmp), .a(a[1]), .b(b[1]), .s(s[1]), .cout(cout_tmp));
 endmodule
-   
+
+/* 4-bit ripple-carry adder that adds two 4-bit numbers (taken from the
+/* ZedBoard's switches) and produces a 4-bit result (displayed on the ZedBoard's
+/* LEDs) */
 module rca4(input wire  [7:0] SWITCH,
             output wire [7:0] LED);
    wire cout0; 

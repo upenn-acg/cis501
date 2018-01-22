87d9205ed25fa587e99e558aad90c36000ca679b
Joe Devietti <joe@devietti.net>
2018-01-22 11:17:22 -0500

    added 'make program' target for programming the FPGA via commandline

--- common/tcl/program.tcl
+++ common/tcl/program.tcl
@@ -1,3 +1,12 @@
+if {![info exists env(BITSTREAM_FILE)]} {
+    puts "BITSTREAM_FILE environment variable isn't set. Don't know what .bit file to use, exiting."
+    exit 1
+}
+if {![file isfile $env(BITSTREAM_FILE)]} {
+    puts "Bitstream file \"$env(BITSTREAM_FILE)\" does not exist, exiting."
+    exit 1
+}
+
 if {[catch open_hw errorString]} {
     puts "Could not open hardware: $errorString"
     exit 1
@@ -8,7 +17,7 @@ if {[catch connect_hw_server errorString]} {
 }
 if {[catch open_hw_target errorString]} {
     puts "Could not open hardware target: $errorString"
-    puts "Is USB cable connected?"
+    puts "Is ZedBoard powered on and USB cable connected?"
     exit 1
 }
 if {[catch current_hw_device xc7z020_1 errorString]} {
@@ -16,11 +25,14 @@ if {[catch current_hw_device xc7z020_1 errorString]} {
     puts "Couldn't set current hardware device: $errorString"
     exit 1
 }
-if {[catch {create_hw_bitstream -hw_device [ current_hw_device ] TODO.bit} errorString]} {
+
+if {[catch {create_hw_bitstream -hw_device [ current_hw_device ] $env(BITSTREAM_FILE)} errorString]} {
     puts "Couldn't associate bitstream with the FPGA: $errorString"
     exit 1
 }
 if {[catch program_hw_devices errorString]} {
     puts "Error programming the FPGA: $errorString"
     exit 1
+} else {
+    puts "FPGA programmed successfully!"
 }

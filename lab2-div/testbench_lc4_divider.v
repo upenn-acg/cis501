dac59aa0e67ba3270df47c47a3be29d0d55595bf
Joe Devietti <joe@devietti.net>
2018-01-28 22:03:19 -0500

    added Natasha and Matt's test cases for lc4_divider_one_iter

--- lab2-div/testbench_lc4_divider.v
+++ lab2-div/testbench_lc4_divider.v
@@ -1,7 +1,9 @@
 /* lc4_divider testbench
- * Milo Martin, CJ Taylor, Benedict Brown, David Mally
- * Updated January 2015
+ * Milo Martin, CJ Taylor, Benedict Brown, David Mally, Matthew Cohen, Natasha Narang
+ * Updated January 2018
  *
+ * Validate 2000 test inputs for the lc4_divider_one_iter module.
+ * 
  * Divide 1000 pairs of random numbers and verify
  * that the quotient and remainder are correct.
  * Then, check division by zero for 1000 random
@@ -13,27 +15,42 @@
 
 `timescale 1ns / 1ps
 
+`define EOF 32'hFFFF_FFFF
+`define NEWLINE 10
+`define NULL 0
+
+`ifndef INPUT
+`define INPUT "test_lc4_divider_one_iter.input"
+`endif
+
 module test_divider;
    // status variables (number of errors and total number of tests)
-   integer     errors, tests;
+   integer     input_file, errors, allTests, divTests;
 
    // input variables (registers so we can generate them here)
    reg [15:0] i_dividend;
    reg [15:0] i_divisor;
-
+   reg [15:0] i_remainder;
+   reg [15:0] i_quotient;
+   
    // output variables (wires because the result comes from the divider outputs)
+   wire [15:0] o_dividend;
    wire [15:0] o_remainder;
    wire [15:0] o_quotient;
 
-   // expected remainder and quotient
+   // expected values
+   reg [15:0] exp_dividend;
    reg [15:0] exp_remainder;
    reg [15:0] exp_quotient;
 
-   // instantiate the Unit Under Test (UUT)
-   lc4_divider div(i_dividend,
-                   i_divisor,
-                   o_remainder,
-                   o_quotient);
+   // instantiate the Units Under Test (UUT)
+   lc4_divider div(.i_dividend(i_dividend), .i_divisor(i_divisor),
+                   .o_remainder(o_remainder), .o_quotient(o_quotient));
+
+   lc4_divider_one_iter divider_iter(.i_dividend(i_dividend), .i_divisor(i_divisor), 
+                                     .i_remainder(i_remainder), .i_quotient(i_quotient), 
+                                     .o_dividend(o_dividend), .o_remainder(o_remainder),
+                                     .o_quotient(o_quotient));
 
 
    initial begin // start testbench block
@@ -41,22 +58,57 @@ module test_divider;
       // initialize inputs
       i_dividend = 0;
       i_divisor = 0;
-      errors = 0;
+      i_remainder = 0;
+      i_quotient = 0;
 
+      errors = 0;
+      allTests = 0; 
+
+      // open the lc4_divider_one_iter test input trace
+      input_file = $fopen(`INPUT, "r");
+      if (input_file == `NULL) begin
+         $display("Error opening file: ", `INPUT);
+         $finish;
+      end
 
       // wait for global reset to finish
       #100;
 
       #2;
 
+      // read in the input trace one line at a time
+      while (7 == $fscanf(input_file, "%b %b %b %b %b %b %b", i_dividend, i_divisor, i_remainder, i_quotient, exp_dividend, exp_remainder, exp_quotient)) begin
+         #8; // wait for the divider to do its thing
+         
+         allTests = allTests + 1;
+         
+         // print an error if one occurred
+         if (o_dividend !== exp_dividend || o_remainder != exp_remainder || o_quotient != exp_quotient) begin
+            errors = errors + 1;
+
+            // break up all binary values into groups of four bits for readability
+            $write("[lc4_divider_one_iter] Error at line %04d: ",    allTests);
+            
+            $display(""); // add a newline
+            //$finish;
+         end
+         
+         #2;
+      end // end while
+      
+      // cleanup
+      if (input_file) begin
+         $fclose(input_file);
+      end
+      
       // loop 2000 times, with a one-indexed number for each test
-      for (tests = 0; tests < 4000;) begin
+      for (divTests = 0; divTests < 4000;) begin
 
          // set random dividend and divisor
          i_dividend = $random;
          i_divisor = $random;
 
-         if(tests >= 2000) begin
+         if (divTests >= 2000) begin
             i_divisor = 16'h0000;
          end
 
@@ -77,35 +129,37 @@ module test_divider;
 
          // check if the expected and computed quotients match; print error otherwise
          if (exp_quotient !== o_quotient) begin
-            $display("Error at test %d: i_dividend = %b %b %b %b (0x%H), i_divisor = %b %b %b %b (0x%H), o_quotient should have been %b %b %b %b (0x%H), but was %b %b %b %b (0x%H) instead",
-                     tests,
+            $display("[lc4_divider] Error at test %d: i_dividend = %b %b %b %b (0x%H), i_divisor = %b %b %b %b (0x%H), o_quotient should have been %b %b %b %b (0x%H), but was %b %b %b %b (0x%H) instead",
+                     divTests,
                      i_dividend[15:12], i_dividend[11:8], i_dividend[7:4], i_dividend[3:0], i_dividend,
                      i_divisor[15:12], i_divisor[11:8], i_divisor[7:4], i_divisor[3:0], i_divisor,
                      exp_quotient[15:12], exp_quotient[11:8], exp_quotient[7:4], exp_quotient[3:0], exp_quotient,
                      o_quotient[15:12], o_quotient[11:8], o_quotient[7:4], o_quotient[3:0], o_quotient);
             errors = errors + 1;
          end
-         tests = tests + 1;
+         divTests = divTests + 1;
+         allTests = allTests + 1; 
 
          // check if the expected and computed remainders match; print error otherwise
          if (exp_remainder !== o_remainder) begin
-            $display("Error at test %d: i_dividend = %b %b %b %b (0x%H), i_divisor = %b %b %b %b (0x%H), o_remainder should have been %b %b %b %b (0x%H), but was %b %b %b %b (0x%H) instead",
-                     tests,
+            $display("[lc4_divider] Error at test %d: i_dividend = %b %b %b %b (0x%H), i_divisor = %b %b %b %b (0x%H), o_remainder should have been %b %b %b %b (0x%H), but was %b %b %b %b (0x%H) instead",
+                     divTests,
                      i_dividend[15:12], i_dividend[11:8], i_dividend[7:4], i_dividend[3:0], i_dividend,
                      i_divisor[15:12], i_divisor[11:8], i_divisor[7:4], i_divisor[3:0], i_divisor,
                      exp_remainder[15:12], exp_remainder[11:8], exp_remainder[7:4], exp_remainder[3:0], exp_remainder,
                      o_remainder[15:12], o_remainder[11:8], o_remainder[7:4], o_remainder[3:0], o_remainder);
             errors = errors + 1;
          end
-         tests = tests + 1;
+         divTests = divTests + 1;
+         allTests = allTests + 1; 
 
          #2; // wait some more
 
       end // end for
 
-      $display("Simulation finished: %d test cases %d errors", tests, errors);
-      $display("<scorePossible>%d</scorePossible>", tests);
-      $display("<scoreActual>%d</scoreActual>", tests - errors);
+      $display("Simulation finished: %d test cases %d errors", allTests, errors);
+      $display("<scorePossible>%d</scorePossible>", allTests);
+      $display("<scoreActual>%d</scoreActual>", allTests - errors);
 
       $finish;
    end

dbc91ba0bf058002d3ab37994fb628a10d1dec5b
Joe Devietti <joe@devietti.net>
2018-02-13 23:30:32 -0500

    better error reporting when TEST_CASE isn't defined, set test case paths via make instead of Verilog now so they work with 'make test' and 'make debug'

--- common/make/vivado.mk
+++ common/make/vivado.mk
@@ -16,9 +16,16 @@ __check_defined = \
     $(if $(value $1),, \
       $(error Undefined $1$(if $2, ($2))))
 
+# variables that should be defined for all labs
 $(call check_defined, SOURCES TOP_SYNTH_MODULE TESTBENCH TOP_TESTBENCH_MODULE, Each lab Makefile should define this)
-# NB: TOP_IMPL_MODULE BITSTREAM_FILENAME CONSTRAINTS are just for labs that run through implementation
-# NB: ZIP_SORUCES ZIP_FILE defined just for labs where multiple source files must be submitted
+
+ifdef TOP_IMPL_MODULE
+$(call check_defined, TOP_IMPL_MODULE BITSTREAM_FILENAME CONSTRAINTS, Each implementation lab Makefile should define this)
+endif
+
+ifdef ZIP_SOURCES
+$(call check_defined, ZIP_SOURCES ZIP_FILE, Each lab Makefile where a zip file is submitted should define this)
+endif
 
 # shorthand variables for constraint files and Tcl scripts
 # NB: COMMON_DIR is wrt the Makefile in each lab's directory, not wrt this file
@@ -27,6 +34,9 @@ XDC_DIR=$(COMMON_DIR)/xdc
 TCL_DIR=$(COMMON_DIR)/tcl
 
 time=time -f "Vivado took %E m:s and %M KB"
+
+# NB: the .set_testcase.v target does create a file .set_testcase.v, but we want it to run every time so we declare it phony
+.PHONY: .set_testcase.v clean extraclean
 
 # if invoked with no explicit target, print out a help message
 .DEFAULT: help
@@ -39,14 +49,14 @@ synth: setup-files $(SOURCES)
 	$(time) vivado -mode batch -source $(TCL_DIR)/build.tcl
 
 # run all tests
-test: $(SOURCES) $(TESTBENCH)
+test: $(SOURCES) $(TESTBENCH) .set_testcase.v
 	rm -rf xsim.dir/
 	echo -n verilog mylib $^ > .prj
 	xelab --define TEST_CASE=\"$(TEST_CASE)\" -cc gcc --debug typical --prj .prj --snapshot snapshot.sim --lib mylib mylib.$(TOP_TESTBENCH_MODULE)
 	xsim snapshot.sim --runall --stats -wdb sim.wdb
 
 # investigate design via GUI debugger
-debug: setup-files
+debug: setup-files .set_testcase.v
 	rm -rf .debug-project
 	vivado -mode batch -source $(TCL_DIR)/debug.tcl
 
@@ -74,10 +84,29 @@ setup-files:
 	echo -n $(CONSTRAINTS) > .constraint-files
 	echo -n $(BITSTREAM_FILENAME) > .bitstream-filename
 
+# find path to this Makefile (NB: MAKEFILE_LIST also contains vivado.mk as the 2nd entry)
+THIS_MAKEFILE_PATH=$(dir $(realpath $(firstword $(MAKEFILE_LIST))))
+
+# write paths to test input/output traces and memory contents
+.set_testcase.v:
+ifdef NEEDS_TEST_CASE
+ifndef TEST_CASE
+	$(error ERROR: you need to define TEST_CASE. Re-run with "TEST_CASE=... make $(MAKECMDGOALS)")
+else
+ifeq (TEST_CASE,)
+	$(error ERROR: you need to define TEST_CASE. Re-run with "TEST_CASE=... make $(MAKECMDGOALS)")
+else
+	echo \`define INPUT_FILE \"$(THIS_MAKEFILE_PATH)test_data/$(TEST_CASE).trace\" > $@
+	echo \`define OUTPUT_FILE \"$(THIS_MAKEFILE_PATH)test_data/$(TEST_CASE).output\" >> $@
+	echo \`define MEMORY_IMAGE_FILE \"$(THIS_MAKEFILE_PATH)test_data/$(TEST_CASE).hex\" >> $@
+endif
+endif
+endif
+
 # remove Vivado logs and our hidden file
 clean:
 	rm -f webtalk*.log webtalk*.jou vivado*.log vivado*.jou xsim*.log xsim*.jou xelab*.log xelab*.jou vivado_pid*.str usage_statistics_webtalk.*ml
-	rm -f .synthesis-source-files .simulation-source-files .ip-blocks .top-synth-module .top-impl-module .top-level-testbench .constraint-files .bitstream-filename .prj
+	rm -f .synthesis-source-files .simulation-source-files .ip-blocks .top-synth-module .top-impl-module .top-level-testbench .set_testcase.v .constraint-files .bitstream-filename .prj
 	rm -rf xsim.dir/ .Xil/ xelab.pb 
 
 # clean, then remove output/ directory: use with caution!

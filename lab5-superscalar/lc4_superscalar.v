41ba091c0e4d3f48b941d330ee5ca7625b072c0d
Joe Devietti <joe@devietti.net>
2018-04-01 17:17:24 -0400

    added skeleton files for superscalar pipeline and regfile

---
+++ lab5-superscalar/lc4_superscalar.v
@@ -0,0 +1,107 @@
+`timescale 1ns / 1ps
+
+// Prevent implicit wire declaration
+`default_nettype none
+
+module lc4_processor(input wire         clk,             // main clock
+                     input wire         rst,             // global reset
+                     input wire         gwe,             // global we for single-step clock
+
+                     output wire [15:0] o_cur_pc,        // address to read from instruction memory
+                     input wire [15:0]  i_cur_insn_A,    // output of instruction memory (pipe A)
+                     input wire [15:0]  i_cur_insn_B,    // output of instruction memory (pipe B)
+
+                     output wire [15:0] o_dmem_addr,     // address to read/write from/to data memory
+                     input wire [15:0]  i_cur_dmem_data, // contents of o_dmem_addr
+                     output wire        o_dmem_we,       // data memory write enable
+                     output wire [15:0] o_dmem_towrite,  // data to write to o_dmem_addr if we is set
+
+                     // testbench signals (always emitted from the WB stage)
+                     output wire [ 1:0] test_stall_A,        // is this a stall cycle?  (0: no stall,
+                     output wire [ 1:0] test_stall_B,        // 1: pipeline stall, 2: branch stall, 3: load stall)
+
+                     output wire [15:0] test_cur_pc_A,       // program counter
+                     output wire [15:0] test_cur_pc_B,
+                     output wire [15:0] test_cur_insn_A,     // instruction bits
+                     output wire [15:0] test_cur_insn_B,
+                     output wire        test_regfile_we_A,   // register file write-enable
+                     output wire        test_regfile_we_B,
+                     output wire [ 2:0] test_regfile_wsel_A, // which register to write
+                     output wire [ 2:0] test_regfile_wsel_B,
+                     output wire [15:0] test_regfile_data_A, // data to write to register file
+                     output wire [15:0] test_regfile_data_B,
+                     output wire        test_nzp_we_A,       // nzp register write enable
+                     output wire        test_nzp_we_B,
+                     output wire [ 2:0] test_nzp_new_bits_A, // new nzp bits
+                     output wire [ 2:0] test_nzp_new_bits_B,
+                     output wire        test_dmem_we_A,      // data memory write enable
+                     output wire        test_dmem_we_B,
+                     output wire [15:0] test_dmem_addr_A,    // address to read/write from/to memory
+                     output wire [15:0] test_dmem_addr_B,
+                     output wire [15:0] test_dmem_data_A,    // data to read/write from/to memory
+                     output wire [15:0] test_dmem_data_B,
+
+                     // zedboard switches/display/leds (ignore if you don't want to control these)
+                     input  wire [ 7:0] switch_data,         // read on/off status of zedboard's 8 switches
+                     output wire [ 7:0] led_data             // set on/off status of zedboard's 8 leds
+                     );
+
+   /***  YOUR CODE HERE ***/
+
+
+
+
+
+
+   /* Add $display(...) calls in the always block below to
+    * print out debug information at the end of every cycle.
+    *
+    * You may also use if statements inside the always block
+    * to conditionally print out information.
+    */
+   always @(posedge gwe) begin
+      // $display("%d %h %h %h %h %h", $time, f_pc, d_pc, e_pc, m_pc, test_cur_pc);
+      // if (o_dmem_we)
+      //   $display("%d STORE %h <= %h", $time, o_dmem_addr, o_dmem_towrite);
+
+      // Start each $display() format string with a %d argument for time
+      // it will make the output easier to read.  Use %b, %h, and %d
+      // for binary, hex, and decimal output of additional variables.
+      // You do not need to add a \n at the end of your format string.
+      // $display("%d ...", $time);
+
+      // Try adding a $display() call that prints out the PCs of
+      // each pipeline stage in hex.  Then you can easily look up the
+      // instructions in the .asm files in test_data.
+
+      // basic if syntax:
+      // if (cond) begin
+      //    ...;
+      //    ...;
+      // end
+
+      // Set a breakpoint on the empty $display() below
+      // to step through your pipeline cycle-by-cycle.
+      // You'll need to rewind the simulation to start
+      // stepping from the beginning.
+
+      // You can also simulate for XXX ns, then set the
+      // breakpoint to start stepping midway through the
+      // testbench.  Use the $time printouts you added above (!)
+      // to figure out when your problem instruction first
+      // enters the fetch stage.  Rewind your simulation,
+      // run it for that many nanoseconds, then set
+      // the breakpoint.
+
+      // In the objects view, you can change the values to
+      // hexadecimal by selecting all signals (Ctrl-A),
+      // then right-click, and select Radix->Hexadecimal.
+
+      // To see the values of wires within a module, select
+      // the module in the hierarchy in the "Scopes" pane.
+      // The Objects pane will update to display the wires
+      // in that module.
+
+      //$display();
+   end
+endmodule

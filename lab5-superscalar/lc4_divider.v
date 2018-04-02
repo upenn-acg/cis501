81b59e49cdd448796c97022040e9af487caf3221
Joe Devietti <joe@devietti.net>
2018-04-01 22:53:12 -0400

    remove accidentally-checked-in symlinks

--- lab5-superscalar/lc4_divider.v
+++ lab5-superscalar/lc4_divider.v
@@ -1,1 +1,25 @@
-/home1/d/devietti/371/solutions/lab2-div/lc4_divider.v
+/* TODO: name and PennKeys of all group members here */
+
+`timescale 1ns / 1ps
+`default_nettype none
+
+module lc4_divider(input  wire [15:0] i_dividend,
+                   input  wire [15:0] i_divisor,
+                   output wire [15:0] o_remainder,
+                   output wire [15:0] o_quotient);
+
+      /*** YOUR CODE HERE ***/
+
+endmodule // lc4_divider
+
+module lc4_divider_one_iter(input  wire [15:0] i_dividend,
+                            input  wire [15:0] i_divisor,
+                            input  wire [15:0] i_remainder,
+                            input  wire [15:0] i_quotient,
+                            output wire [15:0] o_dividend,
+                            output wire [15:0] o_remainder,
+                            output wire [15:0] o_quotient);
+
+      /*** YOUR CODE HERE ***/
+
+endmodule
